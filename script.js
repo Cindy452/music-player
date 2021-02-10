b0fd9794deb98909c5af510dccbec6bd005e86bd
Your Name <you@example.com>
2021-02-10 09:52:41 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,116 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const currentTimeElement = document.getElementById('current-time');
+const durationElement = document.getElementById('duration');
+
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+
+const music = document.querySelector('audio');
+const preBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+const songs = [
+    {
+        name: 'cindy-1',
+        displayName: 'Count your lucky star',
+        artist: 'Cindy'
+    },
+    {
+        name: 'cindy-2',
+        displayName: 'Love and redemption',
+        artist: 'Cindy'
+    },
+    {
+        name: 'cindy-3',
+        displayName: 'Fusion',
+        artist: 'Cindy'
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front row',
+        artist: 'metric'
+    }
+]
+
+let isPlaying = false;
+function playSong() {
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+function pauseSong() {
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()))
+
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+let songIndex = 0;
+
+function preSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+function updateProgressBar(event) {
+    if (music) {
+        const { duration, currentTime } = event.srcElement;
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if (durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        if (durationSeconds) { 
+            durationElement.textContent = `${durationMinutes}:${durationSeconds}`; 
+        }
+
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if (currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+            currentTimeElement.textContent = `${currentMinutes}:${currentSeconds}`; 
+    }
+}
+function setProgressBar(event) {
+  const width = this.clientWidth;
+  const clickX = event.offsetX;
+  const {duration} = music;
+  music.currentTime = (clickX / width) * duration;
+}
+
+
+preBtn.addEventListener('click', preSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
+music.addEventListener('ended', nextSong);
